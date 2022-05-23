735e0ef193d3ffb927cfc3dd67e7f6c84946dff0
Jonny Abrams <jonnyabrams@hotmail.com>
2022-05-23 08:46:06 +0100

    Added land and takeoff functions with tests

--- __test__/airport.test.js
+++ __test__/airport.test.js
@@ -2,9 +2,25 @@ const Airport = require('../src/airport');
 
 describe('Airport', () => {
   const airport = new Airport;
+  const plane = {}
 
   it('is an instance of the Airport class', () => {
     expect(airport).toBeInstanceOf(Airport);
+  });
+
+  describe('land', () => {
+    it('lands a plane at the airport', () => {
+      airport.land(plane);
+      expect(airport.hangar.length).toEqual(1);
+    });
+  });
+
+  describe('takeOff', () => {
+    it('takes off a plane from the airport', () => {
+      console.log(airport.hangar)
+      airport.takeOff(plane);
+      expect(airport.hangar.length).toEqual(0);
+    });
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,17 @@
 class Airport {
+  constructor() {
+    this.hangar = [];
+  }
 
-};
+  land(plane) {
+    this.hangar.push(plane);
+  }
+
+  takeOff(plane) {
+    this.hangar = this.hangar.filter((landedPlane) => {
+      landedPlane !== plane;
+    });
+  }
+}
 
 module.exports = Airport;
